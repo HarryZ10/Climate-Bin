8621e27834567dd7c505de109489d28c38b940b6
hzhu <harryzhu45@gmail.com>
2020-05-17 12:57:41 -0700

    buildpack

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ if __name__ == "__main__":
     # This line is a way to not use https. leave it commented out unless you have a problem
     # os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
     os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
-    # This line lets you run using a fake version of https for a local computer 
-    # You must use https to login to google
+    
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credentials.json"
 
     # app.run(debug=True, ssl_context='adhoc')
