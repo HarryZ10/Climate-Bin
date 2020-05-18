845e523ac47157df1d005372d476150ab01629b5
hzhu <harryzhu45@gmail.com>
2020-05-18 16:38:51 -0700

    update https

--- app/routes/users.py
+++ app/routes/users.py
@@ -68,7 +68,7 @@ def before_request():
     
     if not request.is_secure:
         flash("Redirected to https")
-        return redirect(request.url.replace('http://', 'https://'), 301)
+        return redirect(request.url.replace('http://', 'https://', 1), 301)
     
 
     # Create a list of all the paths that do not need authorization or are part of authorizing
